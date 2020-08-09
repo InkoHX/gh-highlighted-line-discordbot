5d9dbfac220dc9cfe0ea882a384db239ca4ad025
InkoHX <inkohx@gmail.com>
2020-08-09 18:48:11 +0900

    use matchAll

--- src/index.js
+++ src/index.js
@@ -9,44 +9,17 @@ const client = new Client({
   }
 })
 
-const searchHighlightedLink = content => {
-  const HIGHLIGHTED_URL_PATTERN = /https?:\/\/github\.com\/(?<owner>.*)\/(?<repo>.*)\/blob\/(?<branch>.*?)\/(?<path>.*)#L(?<firstLine>[0-9]+)-?L?(?<lastLine>[0-9]+)?/gu
-  const result = []
-  let regexResult
-
-  while ((regexResult = HIGHLIGHTED_URL_PATTERN.exec(content)) !== null) {
-    const {
-      owner,
-      repo,
-      branch,
-      path,
-      firstLine,
-      lastLine
-    } = regexResult.groups
-
-    result.push({
-      owner,
-      repo,
-      branch,
-      path,
-      firstLine,
-      lastLine
-    })
-  }
-
-  return result
-}
-
 client.on('message', message => {
   if (message.author.bot || message.system) return
 
-  const highlightedLinks = searchHighlightedLink(message.content)
+  const highlightedLinks = [...message.content.matchAll(/https?:\/\/github\.com\/(?<owner>.*)\/(?<repo>.*)\/blob\/(?<branch>.*?)\/(?<path>.*)#L(?<firstLine>[0-9]+)-?L?(?<lastLine>[0-9]+)?/gu)]
+    .map(result => result.groups)
   
   Promise.all(highlightedLinks.map(value => fetch(`https://gh-highlighted-line.vercel.app/api/${value.owner}/${value.repo}/${value.branch}/${encodeURIComponent(value.path)}/${value.firstLine}/${value.lastLine ?? ''}`)))
     .then(responses => Promise.all(responses.map(response => response.json())))
     .then(contents => contents.filter(content => content.code.length))
     .then(contents => Promise.all(contents.map(content => message.reply(content.code.join('\n'), { code: content.extension ?? 'livecodeserver', split: true }))))
-    .catch(console.error)
+    .catch(error => message.reply(error.message, { code: 'js' }))
 })
 
 client.login()
